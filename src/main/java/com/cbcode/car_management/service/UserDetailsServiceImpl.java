dc2b861f56e280c026ed3765741c29703d8342c5
lucleber13 <lucleber13@hotmail.com>
2022-07-29 22:56:09 +0100

    Service package with started class

---
+++ src/main/java/com/cbcode/car_management/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.cbcode.car_management.service;
+
+import com.cbcode.car_management.dao.UserRepository;
+import com.cbcode.car_management.model.CustomUserBean;
+import com.cbcode.car_management.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username" + username + " not found"));
+        return CustomUserBean.createInstance(user);
+    }
+}
